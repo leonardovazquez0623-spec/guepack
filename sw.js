c0c1adf29c0f64937fdf4e2b6a476c6ec024f73f
Leonardo Vazquez <leonardo.vazquez0623@gmail.com>
2026-05-30 09:23:13 -0600

    sw.js actualiza cache automaticamente

--- sw.js
+++ sw.js
@@ -1,21 +1,41 @@
-const CACHE_NAME = 'guepack-v2'
+const CACHE_NAME = 'guepack-v3'
 const urlsToCache = [
   '/',
   '/index.html',
   '/login.html',
+  '/admin.html',
+  '/repartidor.html',
+  '/redirect.html',
   '/splash.html',
   '/manifest.json',
   '/Icono_gp.png',
   '/Splash_icon.png'
 ]
+
 self.addEventListener('install', event => {
+  self.skipWaiting()
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   )
 })
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  )
+  self.clients.claim()
+})
+
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone()
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone))
+        return response
+      })
+      .catch(() => caches.match(event.request))
   )
 })
