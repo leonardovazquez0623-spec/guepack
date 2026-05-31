39dcfe32a71b570348da53dcf1eec5a9d62d55dd
Leonardo Vazquez <leonardo.vazquez0623@gmail.com>
2026-05-31 00:12:46 -0600

    firebase push notifications

--- sw.js
+++ sw.js
@@ -1,43 +1,22 @@
-const CACHE_NAME = 'guepack-v4'
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/login.html',
-  '/admin.html',
-  '/repartidor.html',
-  '/redirect.html',
-  '/splash.html',
-  '/manifest.json',
-  '/Icono_gp.png',
-  '/Splash_icon.png'
-]
+importScripts('https://www.gstatic.com/firebasejs/12.14.0/firebase-app-compat.js')
+importScripts('https://www.gstatic.com/firebasejs/12.14.0/firebase-messaging-compat.js')
 
-self.addEventListener('install', event => {
-  self.skipWaiting()
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  )
+firebase.initializeApp({
+  apiKey: "AIzaSyBG3EDbiS-0uswcGGBN9NyrlzpyJFOGnKo",
+  authDomain: "guepack-app.firebaseapp.com",
+  projectId: "guepack-app",
+  storageBucket: "guepack-app.firebasestorage.app",
+  messagingSenderId: "912683525841",
+  appId: "1:912683525841:web:a67f014362160ea29b87f0"
 })
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  )
-  self.clients.claim()
-})
+const messaging = firebase.messaging()
 
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return
-
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const clone = response.clone()
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone))
-        return response
-      })
-      .catch(() => caches.match(event.request))
-  )
+messaging.onBackgroundMessage(payload => {
+  const { title, body } = payload.notification
+  self.registration.showNotification(title, {
+    body,
+    icon: '/Icono_gp.png',
+    badge: '/Icono_gp.png'
+  })
 })
