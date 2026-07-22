26e1377db8348fcd48cf5ed1d3c340d731b675c0
Leonardo Vazquez <leonardo.vazquez0623@gmail.com>
2026-07-21 20:18:24 -0600

    fix conflictos config.js con repartidor y variables globales

--- config.js
+++ config.js
@@ -1,6 +1,10 @@
-const SUPABASE_URL = 'https://zkrnjdsnuyjaxxnluzmn.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_nde3IHFs-CJqqY0w5gV77g_FbtKtH7z'
-const db = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
+if (typeof SUPABASE_URL === 'undefined') {
+  var SUPABASE_URL = 'https://zkrnjdsnuyjaxxnluzmn.supabase.co'
+  var SUPABASE_KEY = 'sb_publishable_nde3IHFs-CJqqY0w5gV77g_FbtKtH7z'
+  var db = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY, {
+    auth: { autoRefreshToken: true, persistSession: true, detectSessionInUrl: false }
+  })
+}
 
 window.SUPABASE_URL = SUPABASE_URL
 window.SUPABASE_KEY = SUPABASE_KEY
